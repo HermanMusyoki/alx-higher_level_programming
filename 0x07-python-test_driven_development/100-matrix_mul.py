b2c90579206768dbee0e97569177c6c71f701daf
HermanMusyoki <salmantso14@gmail.com>
2024-01-10 05:15:21 -0800

    function that multiplies 2 matrices:

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a matrix multiplication function."""
+"""Defines a function for matrix multiplication"""
 
 
 def matrix_mul(m_a, m_b):
@@ -9,12 +9,12 @@ def matrix_mul(m_a, m_b):
         m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
     Raises:
-        TypeError: If either m_a or m_b is not a list of lists of ints/floats.
-        TypeError: If either m_a or m_b is empty.
-        TypeError: If either m_a or m_b has different-sized rows.
-        ValueError: If m_a and m_b cannot be multiplied.
+        TypeError: If m_a or m_b is not a matrix of ints/floats.
+        TypeError: If m_a or m_b is empty.
+        TypeError: If m_a or m_b matrix rows are of different sizes
+        ValueError: If not possible m_a and m_b
     Returns:
-        A new matrix representing the multiplication of m_a by m_b.
+        A new matrix resulting from the multiplication of m_a by m_b.
     """
 
     if m_a == [] or m_a == [[]]:
